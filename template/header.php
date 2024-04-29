f2aa88cde0e0ab0fd4489e76e3b0e6c3220b8fc3
Tryanda Anggita Suwito <arrowos.net@gmail.com>
2024-04-29 23:35:37 +0700

    menghapus comment

--- template/header.php
+++ template/header.php
@@ -12,5 +12,5 @@
     <link href="<?= $main_url ?>assets/sb-admin/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="<?= $main_url ?>assets/img/toga.png" type="image/x-icon">
-    <!-- <link rel="shortcut icon" href="<?= $main_url ?>assets/img/toga.png" type="image/x-icon"> -->
+
 </head>
