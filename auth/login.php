39694a6732b48cecdd04bd020e9c690d0d06f85e
Tryanda Anggita Suwito <arrowos.net@gmail.com>
2024-04-29 23:33:39 +0700

    design ulang page login dan mem-fungsikanya

--- auth/login.php
+++ auth/login.php
@@ -1,121 +1,82 @@
-<?php
-require_once "../config.php";
+ <?php
 
-$sekolah = mysqli_query($koneksi, "SELECT * FROM tbl_sekolah WHERE id = 1"); // Perbaikan penulisan WHERE
-$data = mysqli_fetch_array($sekolah);
+    session_start();
 
-?>
+    if (isset($_SESSION['ssLogin'])) {
+        header("location:../index.php");
+        exit;
+    }
+
+    require_once "../config.php";
+
+    $sekolah = mysqli_query($koneksi, "SELECT * FROM tbl_sekolah WHERE id = 1"); // Perbaikan penulisan WHERE
+    $data = mysqli_fetch_array($sekolah);
+
+    ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - SDN 3 KUJANGSARI</title>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+     <meta name="description" content="" />
+     <meta name="author" content="" />
+     <title>Login - SDN 3 KUJANGSARI</title>
+     <link href="<?= $main_url ?>assets/sb-admin/css/styles.css" rel="stylesheet" />
+     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+     <link rel="icon" type="image/x-icon" href="<?= $main_url ?>assets/img/toga.png">
 
-    <style>
-        body {
-            max-height: 100vh;
-            background-color: #E8F5FC;
+     <!-- <style>
+         #bgLogin {
+             background-image: url("../assets/img/ <?= $data['gambar'] ?>");
+             background-size: cover;
+             background-position: center;
+             background-repeat: no-repeat;
+             height: 100vh;
+         }
+     </style> -->
+ </head>
 
-        }
+ <body class="bg-primary">
+     <div id="layoutAuthentication">
+         <div id="layoutAuthentication_content">
+             <main>
+                 <div class="container mt-4">
+                     <div class="row justify-content-center">
+                         <div class="col-lg-5">
+                             <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                 <div class="card-header">
+                                     <h4 class="text-center font-weight-light my-4">Login - SDN 3 KUJANGSARI</h4>
+                                 </div>
+                                 <div class="card-body">
+                                     <form action="proses-login.php" method="POST">
+                                         <div class="form-floating mb-3">
+                                             <input class="form-control" id="username" name="username" type="text" pattern="[A-Za-z0-9]{3,}" title="Minimal 3 karakter" placeholder="username" autocomplete="off" required />
+                                             <label for="username">username</label>
+                                         </div>
+                                         <div class="form-floating mb-3">
+                                             <input class="form-control" id="inputPassword" type="password" placeholder="Password" minlength="4" name="password" required />
+                                             <label for="inputPassword">Password</label>
+                                         </div>
+                                         <button type="submit" name="login" class="btn btn-primary btn-primary col-12 rounded-pill my-2">Login</button>
+                                     </form>
+                                 </div>
+                                 <div class="card-footer text-center py-3">
+                                     <div class="text-muted smal">Copy Right &copy; SDN 3 KUJANGSARI <?= date("Y") ?></div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+         </div>
+         </main>
 
-        .form {
-            background-color: #ffffff;
-            box-shadow: 10px 10px 0px 1px rgba(82, 137, 220, 1);
-            -webkit-box-shadow: 10px 10px 0px 1px rgba(82, 137, 220, 1);
-            -moz-box-shadow: 10px 10px 0px 1px rgba(82, 137, 220, 1);
-            justify-content: space-between;
-            width: 415px;
-            /* height: 425px; */
-            display: flex;
-            flex-direction: column;
-            gap: 20px;
-            border: 1px solid #568CDD;
-        }
+     </div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+     <script src="<?= $main_url ?>assets/sb-admin/js/scripts.js"></script>
+ </body>
 
-        .copyright {
-            text-align: center;
-            color: #808080;
-            font-size: small;
-        }
-
-        .konten {
-            min-height: 100vh;
-            width: 100vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #e8f5fc;
-        }
-
-        h2 {
-            font-size: 35px;
-            color: #548BDD;
-            text-align: center;
-            margin-top: 50px;
-        }
-
-        input {
-            background-color: #E8F5FC;
-            outline: none;
-            border: none;
-        }
-
-        button {
-            background-color: #568CDD;
-            width: 360px;
-            height: 41px;
-            margin: auto;
-            outline: 1px solid #548BDD;
-            border-left: none;
-            border-top: none;
-            border-right: none;
-            border-bottom: none;
-            color: white;
-            margin-bottom: 100px;
-            cursor: pointer;
-
-        }
-
-        .username,
-        .password {
-            border: 1px solid #548BDD;
-            display: flex;
-            align-items: center;
-            border-bottom: 1px solid #558BDD;
-            background-color: #E8F5FC;
-            width: 350px;
-            height: 41px;
-            justify-content: left;
-            margin: 0px auto;
-            gap: 5px;
-            padding-left: 10px;
-        }
-    </style>
-    <!-- icon -->
-    <script src="https://kit.fontawesome.com/effca873af.js" crossorigin="anonymous"></script>
-</head>
-
-<body>
-    <div class="konten">
-        <form action="proses-login.php" class="form" method="GET" id="login-form">
-            <h2>LOGIN</h2>
-            <div class="username">
-                <i class="fa-solid fa-user"></i>
-                <input type="text" name="username" id="username" placeholder="username" pattern="[A-Za-z0-9]{3,}" title="minimal 3 karakter" autocomplete="off" required />
-            </div>
-            <div class="password">
-                <i class="fa-solid fa-key"></i>
-                <input type="password" name="password" id="inputPassword" placeholder="password">
-            </div>
-            <!-- Your HTML and CSS code -->
-
-            <button type="submit" name="login">LOGIN</button>
-            <p class="copyright">Copyright &copy; SDN 3 KUJANGSARI <?= date("Y") ?></p>
-        </form> <!-- Close the form tag here -->
-</body>
-
-</html>
+ </html>
