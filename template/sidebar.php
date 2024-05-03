0499c66cb7e888062dfff08adc404a743b4142ef
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-05-04 03:14:58 +0700

    edit sidebar

--- template/sidebar.php
+++ template/sidebar.php
@@ -6,35 +6,35 @@
                     <div class="sb-sidenav-menu-heading">Home</div>
                     <a class="nav-link" href="<?= $main_url ?>index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                        Dashboard
+                        DASHBOARD
                     </a>
                     <hr class="mb-0">
                     <div class="sb-sidenav-menu-heading">Admin</div>
                     <a class="nav-link" href="<?= $main_url ?>user/add-user.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></div>
-                        User
+                        USER
                     </a>
                     <a class="nav-link" href="<?= $main_url ?>user/password.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-key"></i></div>
-                        Ganti Password
+                        GANTI PASSWORD
                     </a>
                     <hr class="mb-0">
                     <div class="sb-sidenav-menu-heading">Data</div>
                     <a class="nav-link" href="<?= $main_url ?>siswa/siswa.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-users"></i></div>
-                        Siswa
+                        SISWA
                     </a>
                     <a class="nav-link" href="<?= $main_url ?>guru/guru.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-chalkboard-user"></i></div>
-                        Guru
+                        GURU
                     </a>
                     <a class="nav-link" href="<?= $main_url ?>pelajaran/pelajaran.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-book"></i></div>
-                        Mata Pelajaran
+                        MATA PELAJARAN
                     </a>
-                    <a class="nav-link" href="<?= $main_url ?>index.php">
+                    <a class="nav-link" href="<?= $main_url ?>ujian/ujian.php">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-user-graduate"></i></div>
-                        Ujian
+                        UTS / UAS
                     </a>
 
                     <hr class="mb-0">
