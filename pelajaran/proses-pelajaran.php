8eab7db40e7df5cfc8ecf777f07382bfc47382a3
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-05-04 03:11:14 +0700

    fungsikan proses-pelajaran.php

--- pelajaran/proses-pelajaran.php
+++ pelajaran/proses-pelajaran.php
@@ -23,3 +23,27 @@ if (isset($_POST['simpan'])) {
     header('location: pelajaran.php?msg=added');
     return;
 }
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $pelajaran = $_POST['pelajaran'];
+    $guru = $_POST['guru'];
+
+    $queryPelajaran = mysqli_query($koneksi, "SELECT * FROM tbl_pelajaran WHERE id = $id");
+    $data = mysqli_fetch_array($queryPelajaran);
+    $curPelajaran = $data['pelajaran'];
+
+    $cekPelajaran = mysqli_query($koneksi, "SELECT * FROM tbl_pelajaran WHERE pelajaran = '$pelajaran'");
+
+    if ($pelajaran !== $curPelajaran) {
+        if (mysqli_num_rows($cekPelajaran) > 0) {
+            header('location:pelajaran.php?msg=cancelupdate');
+            return;
+        }
+    }
+
+    mysqli_query($koneksi, "UPDATE tbl_pelajaran SET pelajaran = '$pelajaran', guru = '$guru' WHERE id = $id");
+
+    header('location: pelajaran.php?msg=updated');
+    return;
+}
