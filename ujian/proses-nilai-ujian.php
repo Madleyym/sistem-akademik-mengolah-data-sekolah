ce25476a84fc160bc79c3f8d06db31f71dfcb183
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-06-08 09:17:44 +0700

    memperbaiki nilai ujian fix all

--- ujian/proses-nilai-ujian.php
+++ ujian/proses-nilai-ujian.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
+require_once "../config.php";
+
 if (!isset($_SESSION['ssLogin'])) {
     header("location:../auth/login.php");
     exit;
 }
-
-require_once "../config.php";
-
 
 if (isset($_POST['simpan'])) {
     $noUjian = htmlspecialchars($_POST['noUjian']);
@@ -18,30 +17,39 @@ if (isset($_POST['simpan'])) {
     $max = htmlspecialchars($_POST['max']);
     $avg = htmlspecialchars($_POST['avg']);
 
-    if($min < 50 or $avg < 60){
-     $hasilUjian = "GAGAL";
+    // Periksa apakah NIS sudah ada dalam database
+    $queryCheckNIS = "SELECT COUNT(*) AS count FROM tbl_ujian WHERE nis = '$nis'";
+    $resultCheckNIS = mysqli_query($koneksi, $queryCheckNIS);
+    $rowCheckNIS = mysqli_fetch_assoc($resultCheckNIS);
 
-    } else{
+    if ($rowCheckNIS['count'] > 0) {
+        // Jika NIS sudah digunakan, tampilkan pesan dan kembali ke halaman sebelumnya
+        $msg = "NIS sudah digunakan. Silakan gunakan NIS yang lain.";
+        header("location:nilai-ujian.php?msg=$msg&nis=$nis");
+        exit;
+    }
+
+    // Hitung hasil ujian berdasarkan nilai
+    if ($min < 50 || $avg < 60) {
+        $hasilUjian = "GAGAL";
+    } else {
         $hasilUjian = "LULUS";
     }
 
-  $mapel = $_POST['mapel'];
-  $kls = $_POST['kls'];
-  $nilai = $_POST['nilai'];
-
-  mysqli_query($koneksi, "INSERT INTO tbl_ujian VALUES('$noUjian', '$tgl', '$nis', '$kelas', $sum, $min, $max, $avg, '$hasilUjian')");
-
-  foreach($mapel as $key => $mpl){
-    mysqli_query($koneksi, "INSERT INTO tbl_nilai_ujian (no_ujian, pelajaran, nilai_ujian) 
-                            VALUES ('$noUjian', '$mpl', {$nilai[$key]})");
-  }
+    $mapel = $_POST['mapel'];
+    $nilai = $_POST['nilai'];
 
 
-  header("location:nilai-ujian.php?msg=$hasilUjian&nis=$nis"); 
-  return;
+    mysqli_query($koneksi, "INSERT INTO tbl_ujian VALUES('$noUjian', '$tgl', '$nis', '$kelas', $sum, $min, $max, $avg, '$hasilUjian')");
 
+    foreach ($mapel as $key => $mpl) {
+        $nilaiUjian = $nilai[$key];
+        mysqli_query($koneksi, "INSERT INTO tbl_nilai_ujian (no_ujian, pelajaran, nilai_ujian) 
+                                VALUES ('$noUjian', '$mpl', $nilaiUjian)");
+    }
 
+    // Redirect ke halaman nilai-ujian dengan pesan hasil dan NIS
+    header("location:nilai-ujian.php?msg=$hasilUjian&nis=$nis");
+    exit;
 }
-
-
 ?>
