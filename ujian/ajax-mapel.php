3705821d2bf91d011ba73f6eb2b75046a44ca046
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-06-08 09:13:05 +0700

    fungsikan ajax dengan nilai ujian

--- ujian/ajax-mapel.php
+++ ujian/ajax-mapel.php
@@ -1,28 +1,23 @@
 <?php
 require_once "../config.php";
 
-$kelas = $_GET['kelas'];
+if (isset($_GET['kelas'])) {
+    $kelas = $_GET['kelas'];
 
-$no = 1;
-$queryPelajaran = mysqli_query($koneksi, "SELECT * FROM tbl_pelajaran WHERE kelas = '$kelas'");
+    $queryPelajaran = mysqli_query($koneksi, "SELECT * FROM tbl_pelajaran WHERE kelas = '$kelas'");
 
-// Periksa apakah query berhasil dieksekusi
-if ($queryPelajaran) {
-    // Lakukan iterasi dan tampilkan hasil query
-    while ($data = mysqli_fetch_array($queryPelajaran)) {
-
-
-?>
-        <tr>
-            <td align="center"><?= $no++ ?></td>
-            <td><input type="text" name="kls[]" value="<?= $kelas ?>" class="border-0 bg-transparent col-12 text-center" readonly></td>
-            <td><input type="text" name="mapel[]" value="<?= $data['pelajaran'] ?>" class="border-0 bg-transparent col-12 text-center" readonly></td>
-            <td><input type="number" name="nilai[]" value="0" min="0" max="100" step="5" class="form-control nilai text-center" onchange="fnhitung()"></td>
-        </tr>
-<?php
+    if ($queryPelajaran) {
+        $no = 1;
+        while ($data = mysqli_fetch_array($queryPelajaran)) {
+            echo '<tr>
+                <td align="center">' . $no++ . '</td>
+                <td><input type="text" name="kls[]" value="' . $kelas . '" class="border-0 bg-transparent col-12 text-center" readonly></td>
+                <td><input type="text" name="mapel[]" value="' . $data['pelajaran'] . '" class="border-0 bg-transparent col-12 text-center" readonly></td>
+                <td><input type="number" name="nilai[]" value="0" min="0" max="100" step="5" class="form-control nilai text-center" onchange="fnhitung()"></td>
+            </tr>';
+        }
+    } else {
+        echo "Error: " . mysqli_error($koneksi);
     }
-} else {
-    // Tampilkan pesan kesalahan jika query gagal dieksekusi
-    echo "Error: " . mysqli_error($koneksi);
 }
 ?>
